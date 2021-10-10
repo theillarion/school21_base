0dfa08636a8b082403483b0d959d6799580380ed
Illarion <pro100illarion@gmail.com>
2021-10-10 23:23:09 +0300

    Fix

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 21:24:20 by glashli           #+#    #+#             */
-/*   Updated: 2021/10/10 22:16:05 by glashli          ###   ########.fr       */
+/*   Updated: 2021/10/10 23:19:13 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_striteri(char	*str, void (*f)(unsigned int, char *))
 {
-	size_t	i;
+	unsigned int	i;
 
 	if (f != NULL)
 	{
