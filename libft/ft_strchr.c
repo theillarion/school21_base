16f76640a75e1c951dbde3194e03a3a89e3ca38a
Illarion <pro100illarion@gmail.com>
2021-10-13 23:50:52 +0300

    Fixed bugs

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 00:03:49 by glashli           #+#    #+#             */
-/*   Updated: 2021/10/10 16:29:42 by glashli          ###   ########.fr       */
+/*   Updated: 2021/10/13 23:50:36 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strchr(const char	*str, int symbol)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == symbol)
-			return (&((char *)str)[i]);
+		if (str[i] == (char)symbol)
+			return ((char *)&str[i]);
 		i++;
 	}
-	if (str[i] == '\0' && symbol == '\0')
-		return (&((char *)str)[i]);
+	if (str[i] == '\0' && (char)symbol == '\0')
+		return ((char *)&str[i]);
 	return (NULL);
 }
