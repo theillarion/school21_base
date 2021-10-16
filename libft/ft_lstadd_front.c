2e18872b25ed6ffb50172369093e44e814f728ca
Illarion <pro100illarion@gmail.com>
2021-10-16 22:39:30 +0300

    Changed

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 14:43:15 by glashli           #+#    #+#             */
-/*   Updated: 2021/10/09 14:49:35 by glashli          ###   ########.fr       */
+/*   Updated: 2021/10/14 00:07:48 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	ft_lstadd_front(t_list	**lst, t_list	*new)
 {
-	new->next = *lst;
-	*lst = new;
+	if (lst != NULL)
+	{
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			new->next = *lst;
+			*lst = new;
+		}
+	}
 }
