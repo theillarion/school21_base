18d0a8848f58cfd148c55a8bb79186f93f3f8230
Illarion <pro100illarion@gmail.com>
2022-01-02 23:46:27 +0300

    Simple server-client

--- newproj/minitalk/test/new_client.c
+++ newproj/minitalk/test/new_client.c
@@ -8,15 +8,24 @@ pid_t pid;
 
 void ft_send_data(char	*str)
 {
-	if (length(str) == 0)
-		exit(EXIT_FAILURE);
-	while (str++)
-	{
-		while (*str >> 1)
-		{
+	int	i;
 
+	i = 0;
+	if (strlen(str) == 0)
+		exit(EXIT_FAILURE);
+	while (str[i])
+	{
+		int	j = 128;
+		while (j)
+		{
+			if (j & *str)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			j /= 2;
 		}
-	}
+		i++;
+	} 
 }
 
 int	main(int argc, char	**argv)
--- newproj/minitalk/test/new_server.c
+++ newproj/minitalk/test/new_server.c
@@ -4,8 +4,39 @@
 #include <stdlib.h>
 #include <signal.h>
 
+void one()
+{
+	write(1, "1", 1);
+}
+
+void zero()
+{
+	write(1, "0", 1);
+}
+
 int	main(void)
 {
+	struct sigaction	action_one;
+	struct sigaction	action_zero;
+	sigset_t	set;
+
+	printf("PID: %d\n", getpid());
+	memset(&action_one, 0, sizeof(action_one));
+	action_one.sa_handler = one;
+	sigemptyset(&action_zero.sa_mask);
+	sigaction(SIGUSR1, &action_one, NULL);
+
+	memset(&action_zero, 0, sizeof(action_zero));
+	action_zero.sa_handler = zero;
+	sigemptyset(&action_zero.sa_mask);
+	sigaction(SIGUSR2, &action_zero, NULL);	
 	
+	while(1)
+	{
+		sigaddset(&set, SIGUSR1);
+		sigaddset(&set, SIGUSR2);
+		sigemptyset(&set);
+		pause();
+	}
 	exit(EXIT_SUCCESS);
 }
